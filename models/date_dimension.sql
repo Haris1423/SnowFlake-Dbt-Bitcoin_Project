e371549169d1812837e19cac0a579f26acefb057
harisbhat14231@gmail.com <harisbhat14231@gmail.com>
2026-08-29 19:35:54 +0000

    both weather and date dimension changed from view to table

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -1,3 +1,4 @@
+{{ config(materialized='table') }}
 WITH CTE AS (
     SELECT 
     TO_TIMESTAMP(STARTED_AT) AS STARTED_AT,
@@ -6,7 +7,7 @@ WITH CTE AS (
     {{get_day_type('STARTED_AT')}} DAY_TYPE,
 
      {{get_season('STARTED_AT')}} AS STATION_OF_YEAR
-     
+
          FROM 
     {{ source('demo', 'bike') }}
 )
